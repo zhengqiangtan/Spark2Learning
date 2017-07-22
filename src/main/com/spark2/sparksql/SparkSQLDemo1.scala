0721d412d3fe2a3e690ae6f49e126183c4fd6374
zhengqiangtan <844870676@qq.com>
2017-07-22 21:43:12 +0800

    Initial commit

---
+++ src/main/com/spark2/sparksql/SparkSQLDemo1.scala
@@ -0,0 +1,30 @@
+package com.spark2.sparksql
+
+import org.apache.spark.sql.SparkSession
+
+/**
+  * Created by Administrator on 2017/7/22 0022.
+  */
+object SparkSQLDemo1 {
+  def main(args: Array[String]): Unit = {
+    val spark=SparkSession.builder().appName("test1")
+      .master("local")
+      .config("spark.sql.warehouse.dir","C:\\spark-warehouse")
+      .getOrCreate()
+
+    import spark.implicits._
+    //读取json文件
+    val df=spark.read.json("c://spark-data/people.json")
+    df.show()
+    df.printSchema()
+    df.select("name").show()
+    df.select($"name", $"age" + 1).show()
+    df.filter($"age" > 21).show()
+    df.groupBy("age").count().show()
+
+  //创建临时视图
+  df.createOrReplaceTempView("people")
+  val sqlDF=spark.sql("select name,age from people")
+    sqlDF.show()
+  }
+}
