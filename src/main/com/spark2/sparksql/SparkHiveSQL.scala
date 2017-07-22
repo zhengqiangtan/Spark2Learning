b8bc333e74e302cc06e06110c7259147116167e8
zhengqiangtan <844870676@qq.com>
2017-07-22 22:42:52 +0800

    init commit

---
+++ src/main/com/spark2/sparksql/SparkHiveSQL.scala
@@ -0,0 +1,41 @@
+package com.spark2.sparksql
+import org.apache.spark.sql.{Row, SparkSession}
+
+/**
+  * Created by Administrator on 2017/7/22 0022.
+  */
+object SparkHiveSQL {
+  case class Record(key: Int, value: String)
+  val warehouseLocation = "c://spark-warehouse"
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession
+      .builder()
+      .appName("Spark Hive Example")
+      //.master("local") //不支持本地模式
+      .config("spark.sql.warehouse.dir", warehouseLocation)
+      .enableHiveSupport()//开启hive支持
+      .getOrCreate()
+
+    import spark.implicits._
+    import spark.sql
+
+    sql("CREATE TABLE IF NOT EXISTS src (key INT, value STRING)")
+    sql("LOAD DATA LOCAL INPATH 'kv1.txt' INTO TABLE src")
+    sql("SELECT * FROM src").show()
+    sql("SELECT COUNT(*) FROM src").show()
+
+    val sqlDF = sql("SELECT key, value FROM src WHERE key < 10 ORDER BY key")
+    val stringsDS = sqlDF.map {
+      case Row(key: Int, value: String) => s"Key: $key, Value: $value"
+
+    }
+    stringsDS.show()
+
+    val recordsDF = spark.createDataFrame((1 to 100).map(i => Record(i, s"val_$i")))
+    recordsDF.createOrReplaceTempView("records")
+    sql("SELECT * FROM records r JOIN src s ON r.key = s.key").show()
+
+
+  }
+}
--- src/main/com/spark2/sparksql/SparkSQLDemo1.scala
+++ src/main/com/spark2/sparksql/SparkSQLDemo1.scala
@@ -6,6 +6,7 @@ import org.apache.spark.sql.SparkSession
   * Created by Administrator on 2017/7/22 0022.
   */
 object SparkSQLDemo1 {
+  case class Person(name:String , age:Long)
   def main(args: Array[String]): Unit = {
     val spark=SparkSession.builder().appName("test1")
       .master("local")
@@ -14,17 +15,29 @@ object SparkSQLDemo1 {
 
     import spark.implicits._
     //读取json文件
-    val df=spark.read.json("c://spark-data/people.json")
-    df.show()
-    df.printSchema()
-    df.select("name").show()
-    df.select($"name", $"age" + 1).show()
-    df.filter($"age" > 21).show()
-    df.groupBy("age").count().show()
+//    val df=spark.read.json("c://spark-data/people.json")
+//    df.show()
+//    df.printSchema()
+//    df.select("name").show()
+//    df.select($"name", $"age" + 1).show()
+//    df.filter($"age" > 21).show()
+//    df.groupBy("age").count().show()
 
   //创建临时视图
-  df.createOrReplaceTempView("people")
-  val sqlDF=spark.sql("select name,age from people")
-    sqlDF.show()
+//  df.createOrReplaceTempView("people")
+//  val sqlDF=spark.sql("select name,age from people")
+//    sqlDF.show()
+
+    //DataSet:Typed操作
+//    val caseClassDS=Seq(Person("xiaoqiang",26)).toDS()
+//    caseClassDS.show()
+//
+//    val primitiveDS = Seq(1, 2, 3).toDS()
+//    primitiveDS.map(_ + 1).collect()
+
+    val path = "people.json"
+    val peopleDS = spark.read.json(path).as[Person]
+    peopleDS.show()
+
   }
 }
